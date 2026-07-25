726b6ff31b709981bceefd251cc64150e78c8254
Dani Tabuyo <65305408+dtabuyodesigner@users.noreply.github.com>
2026-07-25 08:03:29 +0200

    Mapa ilustrado de Eslovenia en la pestana Dias

--- eslovenia/sw.js
+++ eslovenia/sw.js
@@ -2,8 +2,8 @@
    Con red: trae siempre la versión más reciente y la guarda.
    Sin red: sirve lo último que se guardó en el móvil. */
 
-const CACHE = "eslovenia26-v6";
-const ARCHIVOS = ["./", "./index.html", "./fotos/bled-osojnica.jpg"];
+const CACHE = "eslovenia26-v7";
+const ARCHIVOS = ["./", "./index.html", "./fotos/bled-osojnica.jpg", "../img/eslovenia-ilustrado.jpg"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting()));
